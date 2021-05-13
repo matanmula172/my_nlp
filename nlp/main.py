e941d31a5885ea7b66aaa6fa1b5c86fc4af3ed8b
matan <matanmula@mail.tau.ac.il>
2021-05-13 13:30:49 +0300

    more functions

--- nlp/main.py
+++ nlp/main.py
@@ -1,17 +1,19 @@
 import nltk
 from transformers import AutoTokenizer
-from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import spacy
+from textblob import TextBlob
 
 
 # nltk.download('punkt')
 # nltk.download('stop')
 # nltk.download('stopwords')
-# nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 
-def tokenize_text(text):
-    return nltk.word_tokenize(text)
+def tokenize_text(text, is_stop_words=False, is_punctuation=False):
+    doc = nlp(text)
+    return [token for token in doc if token.is_stop == is_stop_words and token.is_punct == is_punctuation]
 
 
 def get_sentences(text):
@@ -32,13 +34,48 @@ def decode_huggingface_transformer(encoded_text):
     return tokenizer.decode(encoded_text)
 
 
-def remove_stop_words(text):
-    stop_words = set(stopwords.words('english'))
+def remove_punctuations(text):
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    return tokenizer.tokenize(text)
+
+
+def stem_text(text):
+    stemmer = PorterStemmer()
+    stemmed_tokens = []
     tokenized_text = tokenize_text(text)
-    new_text = []
     for token in tokenized_text:
-        if token not in stop_words:
-            new_text.append(token)
+        stemmed_tokens.append(stemmer.stem(token))
 
-    return " ".join(new_text)
 
+# Lemmatization is the process of converting a word to its base form. The difference between stemming and
+# lemmatization is, lemmatization considers the context and converts the word to its meaningful base form,
+# whereas stemming just removes the last few characters, often leading to incorrect meanings and spelling errors.
+def lemmatize_text(text):
+    doc = nlp(text)
+    return [token.lemma_ for token in doc]
+
+
+def get_words_frequency(text, no_stop_words=True):
+    # Removal of stop words and punctuations
+    tokenized_text = tokenize_text(text, no_stop_words)
+
+    freq_dict = dict()
+    # Calculating frequency count
+    for word in tokenized_text:
+        key = str(word)
+        if key not in freq_dict:
+            freq_dict[key] = 1
+        else:
+            freq_dict[key] += 1
+    return freq_dict
+
+
+def get_n_most_frequent_words(text, n, is_stop_words=False):
+    freq_dict = get_words_frequency(text, is_stop_words)
+    sorted_dict = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
+    return sorted_dict[:n]
+
+
+def text_correction(text):
+    text = TextBlob(text)
+    return text.correct()
