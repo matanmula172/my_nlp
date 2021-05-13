29ac8465a689b31b3ca1e22449094f60bc292172
matan <matanmula@mail.tau.ac.il>
2021-05-13 10:02:17 +0300

    Initial commit

---
+++ nlp/main.py
@@ -0,0 +1,31 @@
+import nltk
+from transformers import AutoTokenizer
+import spacy
+
+
+# nltk.download('punkt')
+# nltk.download('stop')
+# nltk.download('stopwords')
+# nlp = spacy.load("en_core_web_sm")
+
+
+def tokenize_text(text):
+    return nltk.word_tokenize(text)
+
+
+def get_sentences(text):
+    return nltk.sent_tokenize(text)
+
+
+# bert-base-uncased is a masked language model
+# Masked Language Modeling is a fill-in-the-blank task, where a model
+# uses the context words surrounding a mask token to try to predict what the masked word should be
+# don't know why this model is the best for the encoding task
+def encode_huggingface_transformer(text):
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    return tokenizer.encode(text)
+
+
+def decode_huggingface_transformer(encoded_text):
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    return tokenizer.decode(encoded_text)
